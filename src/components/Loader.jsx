3306499bd567111aba88a3ab7f2e6563beddf544
Marie Mélody F <mariemelo@MacBook-Pro-de-Marie.local>
2023-09-03 20:32:56 +0200

    favorites-ok

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,19 +2,17 @@ const Loader = () => {
   return (
     <div className="scene">
       <div className="cube-wrapper">
-        <a href="https://www.rareprogrammer.com/bouncing-cube-css-loader">
-          <div className="cube">
-            <div className="cube-faces">
-              <div className="cube-face shadow"></div>
-              <div className="cube-face bottom"></div>
-              <div className="cube-face top"></div>
-              <div className="cube-face left"></div>
-              <div className="cube-face right"></div>
-              <div className="cube-face back"></div>
-              <div className="cube-face front"></div>
-            </div>
+        <div className="cube">
+          <div className="cube-faces">
+            <div className="cube-face shadow"></div>
+            <div className="cube-face bottom"></div>
+            <div className="cube-face top"></div>
+            <div className="cube-face left"></div>
+            <div className="cube-face right"></div>
+            <div className="cube-face back"></div>
+            <div className="cube-face front"></div>
           </div>
-        </a>
+        </div>
       </div>
     </div>
   );
